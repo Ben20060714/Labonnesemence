80c8d83edc9a884ab853b535d9da9d9e4b97b8b7
Ben20060714 <benkailazad8@gmail.com>
2026-06-03 08:57:16 +0100

    Corrected Vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Labonnesemence',
+    base: '/Labonnesemence/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
